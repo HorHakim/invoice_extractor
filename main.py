5d1aa133373b5f2551655da205505820923587d3
Hakim Horairy <horairyhakim@gmail.com>
2025-05-21 10:39:02 +0200

    v1.1

--- main.py
+++ main.py
@@ -29,7 +29,21 @@ if uploaded_image:
     base64_image = encode_image(image_bytes)
 
     st.image(image_bytes, caption="Image chargée", use_container_width =True)
+    context = """
+    Tu es un assistant très précis spécialisé dans l'extraction de données depuis des images de factures. Analyse attentivement la facture fournie et extrais uniquement les trois informations suivantes :
 
+    Montant total (le montant total TTC facturé, incluant clairement la devise utilisée).
+
+    Date de la facture (la date d'émission ou date inscrite explicitement comme date de facture au format JJ/MM/AAAA).
+
+    Nom du fournisseur (le nom complet du fournisseur tel qu'il est explicitement mentionné en haut de la facture).
+
+    Date d'échéance (au format JJ/MM/AAAA))
+
+    Sois extrêmement attentif et précis, particulièrement si le texte est manuscrit, car certaines lettres ou chiffres pourraient être difficiles à interpréter. Si une donnée n'est pas clairement lisible ou absente, précise explicitement : "Information non lisible" ou "Information absente".
+
+    Ne fournis aucune information supplémentaire ni de commentaire superflu. Ta réponse doit strictement respecter le format 
+    """
     prompt = """
     Extrais clairement les informations suivantes de cette facture si elles sont disponibles :
     - Montant total
@@ -46,6 +60,10 @@ if uploaded_image:
 
     # Construction des messages pour Pixtral
     messages = [
+        {
+            "role" : "system",
+            "content" : context
+        },
         {
             "role": "user",
             "content": [
